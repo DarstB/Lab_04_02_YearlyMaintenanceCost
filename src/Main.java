7e769d327a4fc2cf7f7beb1e811b699b4ce1e5dc
DarstB <bradendarst10@gmail.com>
2024-01-31 10:14:06 -0500

    Lab 04 Task 2

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        double springCost = 302.32;
+        double summerCost = 278.54;
+        double fallCost = 293.98;
+        double winterCost = 288.66;
+        double totalCost = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("Your spring cost is $" + springCost);
+        System.out.println("Your summer cost is $" + summerCost);
+        System.out.println("Your fall cost is $" + fallCost);
+        System.out.println("Your winter cost is $" + winterCost);
+        System.out.println("This makes your total yearly cost $" + totalCost);
+
+    }
+}
